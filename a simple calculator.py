34a22dd9fd8c93a0668e135bedceeddb9b5749af
richard-thangsing <richardthangsing@gmail.com>
2025-10-02 22:13:07 +0530

    a very basic calculator using Python

---
+++ a simple calculator.py
@@ -0,0 +1,18 @@
+#This is a basic calculator using Python
+
+number1 = int(input('Enter a number: ')) #we use the input function to enter both our numbers
+number2 = int(input('Enter another number: ')) # prefixing it with the int(integer),
+# our inputs will only take integers as values
+
+sum = number1 + number2
+print('The sum of both the numbers is:', sum)
+
+subtraction = number1 - number2
+print('The subtraction of both the numbers is:', subtraction)
+
+multiplication = number1 * number2
+print('The multiplication of both the numbers is:', multiplication)
+
+division = number1 / number2
+print('The division of both the numbers is:', division)
+
