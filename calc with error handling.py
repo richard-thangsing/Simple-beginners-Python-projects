4f01f9f0e2f7395e73ccd639289be520c2880df7
richard-thangsing <richardthangsing@gmail.com>
2025-10-06 12:47:18 +0530

    calc added

--- calc with error handling.py
+++ calc with error handling.py
@@ -1,1 +1,17 @@
 
+num1 = float(input('Enter the first number: '))
+op = input('Enter the operator: (+, -, *, /): ')
+num2 = float(input('Enter the second number: '))
+if op == '+':
+    print(num1 + num2)
+elif op == '-':
+    print(num1 - num2)
+elif op == '*':
+    print(num1 * num2)
+elif op == '/':
+    if num2 != 0: # this checks if the 2nd no is 0 or not since we can divide a number by zero
+        print(num1 / num2)
+    else:
+        print('ZeroDivisionError')
+else:
+    print('Invalid operator')
